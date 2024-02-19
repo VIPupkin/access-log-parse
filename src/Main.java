d69206f8b4ee86ee0f5b31cd5c1a3ad6d231592c
Vasiliy Pupkin <v.i.pupkin@mail.ru>
2024-02-19 19:18:41 +0300

    добавлено задание по теме ""Синтаксис языка" и “Базовые типы данных”"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите 1ое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите 2ое число: ");
+        int number2 = new Scanner(System.in).nextInt();
 
+        System.out.println("Сумма: "+ (number1+number2));
+        System.out.println("Разность: "+ (number1-number2));
+        System.out.println("Произведение: "+ (number1*number2));
+        System.out.println("Частное: "+((double)number1/number2));
     }
 }
