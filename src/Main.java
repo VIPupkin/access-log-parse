c35ba4a8884960717422eed336fffdd0380143d5
Vasiliy Pupkin <v.i.pupkin@mail.ru>
2024-03-09 21:44:37 +0300

    добавлен бесконечный цикл с вводом файла журнала, проверкой на существование файла, проверкой на каталог и счетчиком введеных файлов

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста :" +text.length());
+
+        int cnt =1;
+        while (true){
+            System.out.println("Введите имя файла с полным путём:");
+            String path=  new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory=file.isDirectory();
+            if (!fileExists){
+                System.out.println("Файл не существует.");
+                continue;
+            }
+            if (isDirectory){
+                System.out.println("Это не файл, а каталог.");
+                continue;
+            }
+            {
+                System.out.println("Путь указан верно.");
+                System.out.println("Это файл номер "+cnt);
+                cnt++;
+            }
+        }
     }
 }
